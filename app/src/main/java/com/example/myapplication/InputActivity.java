dd7399fd9a4d03ecb5c0f5935551f6e1955dfe48
ailoyu <quangtrinhhuynh02@gmail.com>
2023-05-05 09:07:59 +0700

    Code lần 3

--- app/src/main/java/com/example/myapplication/InputActivity.java
+++ app/src/main/java/com/example/myapplication/InputActivity.java
@@ -2,10 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -23,7 +28,20 @@ public class InputActivity extends AppCompatActivity {
         bt_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            
+                String filename = et_name.getText().toString();
+
+                String cont = "";
+                cont +=  "Name : " + et_name.getText().toString();
+                cont += "\t";
+                cont += "Price: " + et_price.getText().toString();
+                try {
+                    FileOutputStream fout = openFileOutput(filename, Context.MODE_PRIVATE);
+                    fout.write(cont.getBytes());
+                    fout.close();
+                    Toast.makeText(getBaseContext(), "Save successfully!", Toast.LENGTH_SHORT).show();
+                }catch (IOException ex){
+                    Toast.makeText(getBaseContext(), "Open Error!", Toast.LENGTH_SHORT).show();
+                };
             }
         });
 
--- app/src/main/java/com/example/myapplication/OutputActivity.java
+++ app/src/main/java/com/example/myapplication/OutputActivity.java
@@ -3,12 +3,46 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class OutputActivity extends AppCompatActivity {
+    String[] bookList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
+
+        ListView list = (ListView) findViewById(R.id.listview);
+        Button bt_view = (Button) findViewById(R.id.bt_view);
+        TextView tv_view = (TextView) findViewById(R.id.tv_view);
+
+        bookList = fileList();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, bookList);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String data = "";
+                int c;
+                try{
+                    FileInputStream fin = openFileInput(bookList[i]);
+                    while((c = fin.read()) != -1)
+                        data += Character.toString((char)c);
+                    fin.close();
+                    tv_view.setText(data);
+                }catch(IOException ex){};
+            }
+        });
     }
 }
